23824f242d61bbd75e10a6603c3ddbc74b4e15d3
Kota <maedakoutaa@gmail.com>
2022-04-10 13:21:01 +0900

    commit

---
+++ MiniApp44-harfModal/HalfModalViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HalfModalViewController.swift
+//  MiniApp44-harfModal
+//
+//  Created by 前田航汰 on 2022/04/10.
+//
+
+import UIKit
+
+class HalfModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- MiniApp44-harfModal/ViewController.swift
+++ MiniApp44-harfModal/ViewController.swift
@@ -11,9 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let next = segue.destination
+        if let sheet = next.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            //モーダル出現後も親ビュー操作可能にする
+            sheet.largestUndimmedDetentIdentifier = .medium
+            // 角丸の半径を変更する
+            sheet.preferredCornerRadius = 40.0
+            //　グラバーを表示する
+            sheet.prefersGrabberVisible = true
+        }
+    }
 }
-
